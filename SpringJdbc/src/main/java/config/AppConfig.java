99429e4036bfebb55bfad8e8964af114fdfa566a
gershomlapaix <nsengiyumv33@gmail.com>
2024-02-21 16:48:01 +0200

    description

---
+++ SpringJdbc/src/main/java/config/AppConfig.java
@@ -0,0 +1,56 @@
+/*
+* Class description
+*
+* Annotations:
+* ----
+* @Configuration: indicates that this class provides Spring context configuration
+* @ComponentScan: instructs spring to scan the 'com.lg' package, and it's sub packages for spring
+* components(beans) to be managed by Spring
+*@PropertySource("classpath:database.properties") : specifies the location of "database.properties" file that contains
+* configuration properties
+*
+* Environment environment: provided by spring, it holds profiles and properties from the application context
+* URL, DRIVER, PASSWORD, USER: the keys that help to fetch values from 'database.properties'
+*
+* */
+
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * Application configuration file
+ * @author Gershom Nsengiyumva
+ * */
+@Configuration
+@ComponentScan("com.lg")
+@PropertySource("classpath:database.properties")
+public class AppConfig {
+
+    @Autowired
+    Environment  environment;
+
+    private final String URL="url";
+    private final String USER = "dbuser";
+    private final String DRIVER = "driver";
+    private final String PASSWORD = "dbpassword";
+
+    @Bean
+    DataSource dataSource(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(environment.getProperty(URL));
+        driverManagerDataSource.setUsername(environment.getProperty(USER));
+        driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
+        driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
+
+        return driverManagerDataSource; // returned as spring managed bean
+    }
+}
---
+++ SpringJdbc/src/main/java/model/Person.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Person {
+    private Long id;
+    private Integer age;
+    private String firstName, lastName;
+
+
+    // contractors
+    public Person(){}
+    public Person(Long id, Integer age, String firstName, String lastName){
+        this.id = id;
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+//    toString
+    @Override
+    public String toString(){
+        return "Person{"+ "id="+ id + ", age="+ age +", name="+ firstName +" "+ lastName + '\'' + "}";
+    }
+}
